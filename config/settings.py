ebe3c31f10ad26565201e8404f5b3633d80a9647
Alan Cheung <licentia300@gmail.com>
2026-08-11 11:16:21 +0800

    start complete

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
