2cf85167ca91971cd2abd9b238aa894b821f2f50
Alan Cheung <licentia300@gmail.com>
2026-08-11 14:28:58 +0800

    update

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project_data',        # ← 改成这个，和 pgAdmin 里一致
+        'USER': 'postgres',
+        'PASSWORD': '1234',        # 确认密码正确
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
