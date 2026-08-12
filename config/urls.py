94b9123dcffc9ab8b7b0c1dc584c51a12d9c0c59
Dannycoding2026 <wahkeunglung@gmail.com>
2026-08-12 14:14:12 +0800

    Add listings feature: wallpaper list & detail pages, views, urls, templates and move partials

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('pages.urls')),                
+    path('listings/', include('listings.urls', namespace='listings')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
+
+# Important for media files (images)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
