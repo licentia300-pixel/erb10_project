6e6f029dbd18c6d5288401f75bd638b395b535f4
Alan Cheung <licentia300@gmail.com>
2026-08-12 14:40:09 +0800

    correct for merge final

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls', namespace='pages')),
-    path('about/', include('pages.urls', namespace='about')),
+    #path('', include('pages.urls', namespace='pages')),
+    #path('about/', include('pages.urls', namespace='about')),
     path('accounts/',include('accounts.urls', namespace='accounts')),
 ]
